e4e8792d342d30198242617b30d088318055e705
Hackbright Student <user@mini-005.(none)>
2014-02-11 12:30:47 -0800

    Not quite working properly. Not incrementing the readlines

--- sorted_data.py
+++ sorted_data.py
@@ -1,24 +1,41 @@
+#setting argv to take our script, filename
 from sys import argv
-#import the list of restaurants (scores.txt)
 script, filename = argv
 
-#open the file
+#opens the given file
 opened_file = open(filename, 'r')
 
-#read the file
+#reads given file, line by line
 read_file = opened_file.readline()
 
-#make a function to run it on every line
-def process_line(read_file):
-    #put the working code here!
-    pass
-#split the file into a list
-split_restaurants = read_file.split(":")
-#print split_restaurants
-print split_restaurants
+#create an empty dict. we will feed restaurant:rating pairs to later
+restaurants_ratings = {}
 
-#create an empty dict
-#restaurants = {}
+#this function returns clean lines from raw, to:
+#optimized lines as lists with 2 items ["key", "value"]
+def process_line(read_file):
+    #strip the whitespace in each line
+    stripped_restaurants = read_file.strip()
+
+    #optimizing the items in the line -- splitting by colon
+    #will return 1 list w/ 2 items ["name", "rating"]
+    optimized_line = stripped_restaurants.split(":")
+
+    #takes 'optimized_line' and feeds it as an entry
+    #to the dictionary (restaurant_ratings)
+    #as key:value pairs.
+    restaurants_ratings[optimized_line[0]] = restaurants_ratings.get(optimized_line[0], optimized_line[1]) 
+    #prints dictionary, with new entry
+    print restaurants_ratings
+
+# for every line in the file do this function 
+while #items in read_file != None:
+    #perform the function on that line    
+    process_line(read_file)
+
+print restaurants_ratings
+
+
 
 #feed content into the empty dict in key value pairs
 #Strip the whitespace per line item
@@ -28,4 +45,3 @@ print split_restaurants
 #print "Restaurant '%s' is rated at %r" % (key, value)
 
 #close the file
-opened_file.close()
